2e17b487f2ebf2ad072c40b62aa4d3e8ab144fab
L1MeN9Yu <baal998.mophisto@gmail.com>
2019-04-26 15:39:15 +0800

    Add Swift Project

---
+++ iOS/Swift/Panigale/Panigale/Panigale.h
@@ -0,0 +1,19 @@
+//
+//  Panigale.h
+//  Panigale
+//
+//  Created by Mengyu Li on 2019/4/26.
+//  Copyright © 2019 L1MeN9Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Panigale.
+FOUNDATION_EXPORT double PanigaleVersionNumber;
+
+//! Project version string for Panigale.
+FOUNDATION_EXPORT const unsigned char PanigaleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Panigale/PublicHeader.h>
+
+
